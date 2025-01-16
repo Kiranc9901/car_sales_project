8719d1533a223975dfbe5d2664fae211223c3640
KIRAN C <89858682+Kiranc9901@users.noreply.github.com>
2025-01-17 02:15:48 +0530

    Update stored_procedure.sql

--- DDL/stored_procedure.sql
+++ DDL/stored_procedure.sql
@@ -1,1 +1,9 @@
-
+create procedure updatewatermarktabke
+ @lastload varchar(200)
+as
+begin
+  begin transaction;
+  update [dbo].[water_table]
+  set last_load = @lastload
+	commit transaction;
+end ;
