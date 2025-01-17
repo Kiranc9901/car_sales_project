e82b7d3d413737afb5e837529cf717caed19f446
KIRAN C <89858682+Kiranc9901@users.noreply.github.com>
2025-01-17 22:52:55 +0530

    Update ADLS_Databrics_mount.py

--- databricks/ADLS_Databrics_mount.py
+++ databricks/ADLS_Databrics_mount.py
@@ -12,3 +12,10 @@ dbutils.fs.mount( source = 'wasbs://bronze@kc123.blob.core.windows.net',
 
 dbutils.fs.mount( source = 'wasbs://silver@kc123.blob.core.windows.net', 
                  mount_point= '/mnt/silver/kc12', extra_configs ={'fs.azure.sas.silver.kc123.blob.core.windows.net':'sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-01-17T02:09:55Z&st=2025-01-16T18:09:55Z&spr=https&sig=3PFo8OhhLwpkeAAqLZlUexTQyqwDe0%2B%2BDmctosHS8Ug%3D'})
+
+
+#mounting adls with databticks for gold layer container
+
+dbutils.fs.mount( source = 'wasbs://gold@kc123.blob.core.windows.net', 
+                 mount_point= '/mnt/gold/kc12', extra_configs ={'fs.azure.sas.gold.kc123.blob.core.windows.net':'sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2025-01-19T20:15:35Z&st=2025-01-17T12:15:35Z&spr=https&sig=hHshdAsZ8Iw07e91peukoTTAiK27MtrfYPTuuffPf%2B0%3D'})
+
